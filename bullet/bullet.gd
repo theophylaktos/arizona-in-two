aeed743941336b5de209f74b34ff9b92e3f42e16
Justbetter14 <145606090+Justbetter14@users.noreply.github.com>
2026-03-27 16:13:45 -0400

    Add bullet bounce logic and raycasts
    Added a bounce mechnaic for the bullets and made the max bounces be variable

--- bullet/bullet.gd
+++ bullet/bullet.gd
@@ -7,9 +7,42 @@ extends Area2D
 
 var speed = 128 #The speed of the bullet
 
+var bounceCount = 0
+var maxBounceCount = 2
+
 func _physics_process(delta):
 	position += speed * direction * delta
 
-
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	call_deferred("queue_free")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name != "Player":
+		var angle: int = int(rad_to_deg(body.get_angle_to(direction)))
+		if angle == 135:
+			if $CollisionShape2D/Left.is_colliding():
+				direction = Vector2(-direction.x, direction.y)
+			elif $CollisionShape2D/Down.is_colliding():
+				direction = Vector2(direction.x, -direction.y)
+		elif angle == -135:
+			if $CollisionShape2D/Left.is_colliding():
+				direction = Vector2(-direction.x, direction.y)
+			elif $CollisionShape2D/Up.is_colliding():
+				direction = Vector2(direction.x, -direction.y)
+		elif angle == 45:
+			if $CollisionShape2D/Right.is_colliding():
+				direction = Vector2(-direction.x, direction.y)
+			elif $CollisionShape2D/Down.is_colliding():
+				direction = Vector2(direction.x, -direction.y)
+		elif angle == -45:
+			if $CollisionShape2D/Right.is_colliding():
+				direction = Vector2(-direction.x, direction.y)
+			elif $CollisionShape2D/Up.is_colliding():
+				direction = Vector2(direction.x, -direction.y)
+		elif abs(angle) == 90 or angle == 180 or angle == 0:
+			var dirConversions = {0: Vector2(-1, 0), 180: Vector2(1, 0), -90: Vector2(0,1), 90: Vector2(0,-1)}
+			direction = dirConversions[angle]
+		if maxBounceCount < bounceCount:
+			call_deferred("queue_free")
+		await get_tree().create_timer(0.05).timeout
+		
